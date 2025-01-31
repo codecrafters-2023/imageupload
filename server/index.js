82659b6ec06d9a92d4e36cbe1d1e2a98ea3bd7aa
codecrafters-2023 <145508415+codecrafters-2023@users.noreply.github.com>
2025-02-01 04:57:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:3000',
+    // origin: 'http://localhost:3000',
+    origin: 'https://imagesupload.vercel.app',
     credentials: true
 }));
 app.use(express.json());
